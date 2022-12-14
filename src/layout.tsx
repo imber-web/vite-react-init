d9b466a532f9a580e1f55fa106b2033b29fdb4de
imber <a1157911285@163.com>
2022-12-14 22:03:05 +0800

    feat: 测试提交

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,23 +1,39 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
 import { Spin } from 'antd'
 import { Suspense } from 'react'
 import { toggleTheme } from './utils'
 
 export default function Layout() {
+  const navigate = useNavigate()
   return (
     <div className="layout">
       <div id="sidebar">
         <ul>
-          <li>
-            <Link to={`home/1`}>params参数</Link>
+          <li
+            onClick={() => {
+              navigate('home/1')
+            }}
+          >
+            {/* <Link to={`home/1`}>params参数</Link> */}
+            params参数
           </li>
-          <li>
-            <Link to={`about?type=2`}>search参数</Link>
+          <li
+            onClick={() => {
+              navigate('about?type=2')
+            }}
+          >
+            {/* <Link to={`about?type=2`}>search参数</Link> */}
+            search参数
           </li>
-          <li>
-            <Link to={`rank`} state={{ status: 3 }}>
+          <li
+            onClick={() => {
+              navigate('rank', { state: { status: 3 } })
+            }}
+          >
+            {/* <Link to={`rank`} state={{ status: 3 }}>
               state参数
-            </Link>
+            </Link> */}
+            state参数
           </li>
           <li>
             <Link to={`react-demo`}>react-demo</Link>
